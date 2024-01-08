7cb3cb58397ae52be3e8d215ec6649fde458fafa
Ian Shi <Ianshi1026@gmail.com>
2024-01-07 20:28:00 -0500

    working site

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function moveButton() {
+    document.getElementById("no").style.top = (Math.random() * 80 + 10) + "%";
+    document.getElementById("no").style.left = (Math.random() * 80 + 10) + "%";
+}
